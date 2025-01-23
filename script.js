ae33509de511a1bcb51b681f7c511b966128e2b3
Gustavo <montenegro12gustavo@gmail.com>
2025-01-23 14:24:39 -0300

    inicialização do site

---
+++ script.js
@@ -0,0 +1,8 @@
+const formulario = document.querySelector("form");
+const topicos = document.querySelector(".topicos");
+
+formulario.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    console.log(topicos.value);
+})
